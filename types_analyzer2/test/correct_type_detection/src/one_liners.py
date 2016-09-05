1fb582f2893b096617bfa9b6f43e665001154f82
Victor Yacovlev <v.yacovlev@gmail.com>
2016-09-05 19:00:24 +0300

    Added multidimension list comrehensions test

--- types_analyzer2/test/correct_type_detection/src/one_liners.py
+++ types_analyzer2/test/correct_type_detection/src/one_liners.py
@@ -8,3 +8,4 @@ numbers = [i for i in range(0, 10)]  # expects list<int>
 pairs = [(x,y) for x in range(10) for y in range(5)]  # expects list<(int, int)>
 prods = [x*y for x in range(10) for y in range(5)]  # expects list<int>
 prods_non_zero = [x*y for x in range(10) for y in range(5) if x>0 and y>0]  # expects list<int>
+two_dim = [[y for y in range(5)] for _ in range(4)]  # expects list<list<int>>
