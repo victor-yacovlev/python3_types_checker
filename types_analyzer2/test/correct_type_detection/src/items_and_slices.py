967c9d0007deb7bbef7e5b9414a2db616e3856b8
Victor Yacovlev <v.yacovlev@gmail.com>
2016-09-05 14:31:03 +0300

    Added mapping to index test

--- types_analyzer2/test/correct_type_detection/src/items_and_slices.py
+++ types_analyzer2/test/correct_type_detection/src/items_and_slices.py
@@ -11,3 +11,17 @@ s2 = s[1:2]  # expects: str
 s3 = s[:2]  # expects: str
 s4 = s[1:]  # expects: str
 s5 = s[:]  # expects: str
+
+ssdict = { "a": "b", "c": "d" }
+ss1 = ssdict["a"]  # expects: str
+
+sidict = { "a": 1, "b": 2 }
+si1 = sidict["a"]  # expects: int
+
+sodict = { "a": "b", "c": 1 }
+# Value of mixed-value-type mapping is a generic object
+so1 = sodict["a"]  # expects: object
+
+sndict = { "a": 1, "b": 1.2 }
+# Value of mixed-value-type mapping is a common value base
+sn1 = sndict["a"]  # expects: _numeric
