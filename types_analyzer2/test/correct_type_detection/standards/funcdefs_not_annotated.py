4c4dd8bdac91574356daa57eb5c7f4b8dace25d8
Victor Yacovlev <v.yacovlev@gmail.com>
2016-09-06 12:06:22 +0300

    Reorganized tests

--- types_analyzer2/test/correct_type_detection/standards/funcdefs_not_annotated.py
+++
@@ -1,6 +0,0 @@
-## Function that accepts something can be added to each other and returns it's summ
-def summ(a, b):
-    return a + b
-
-summ_1 = summ(1, 2)  # expects: int  ### summ_1: int
-summ_2 = summ("abc", "def")  # expects: str  ### summ_2: str
